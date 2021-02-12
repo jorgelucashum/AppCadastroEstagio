5c76c6f23d0cb179039245e31d018f6fc3c9b70d
jorgeucas lucas <jorge_dofus1@hotmail.com>
2021-02-12 19:02:53 -0300

    Add project files.

---
+++ AppCadastroEstagio/Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿
+namespace AppCadastroEstagio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labID = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridViewCliente = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtDDD = new System.Windows.Forms.TextBox();
+            this.dataGridViewTelefone = new System.Windows.Forms.DataGridView();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.notifyIcon2 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.tELEFONEBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dDDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tELEFONE1DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cLIENTEBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nOMEDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Deletar = new System.Windows.Forms.DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCliente)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTelefone)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tELEFONEBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cLIENTEBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labID
+            // 
+            this.labID.AutoSize = true;
+            this.labID.Location = new System.Drawing.Point(12, 22);
+            this.labID.Name = "labID";
+            this.labID.Size = new System.Drawing.Size(21, 13);
+            this.labID.TabIndex = 0;
+            this.labID.Text = "ID:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(63, 91);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Adicionar Telefone";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridViewCliente
+            // 
+            this.dataGridViewCliente.AllowUserToAddRows = false;
+            this.dataGridViewCliente.AllowUserToDeleteRows = false;
+            this.dataGridViewCliente.AutoGenerateColumns = false;
+            this.dataGridViewCliente.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewCliente.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iDDataGridViewTextBoxColumn,
+            this.nOMEDataGridViewTextBoxColumn,
+            this.Deletar});
+            this.dataGridViewCliente.DataSource = this.cLIENTEBindingSource;
+            this.dataGridViewCliente.Location = new System.Drawing.Point(111, 278);
+            this.dataGridViewCliente.Name = "dataGridViewCliente";
+            this.dataGridViewCliente.Size = new System.Drawing.Size(303, 133);
+            this.dataGridViewCliente.TabIndex = 2;
+            this.dataGridViewCliente.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewCliente_CellClick);
+            this.dataGridViewCliente.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewCliente_CellContentClick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtDDD);
+            this.panel1.Controls.Add(this.dataGridViewTelefone);
+            this.panel1.Controls.Add(this.txtTelefone);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtNome);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtID);
+            this.panel1.Controls.Add(this.labID);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(484, 219);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // txtDDD
+            // 
+            this.txtDDD.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.tELEFONEBindingSource, "DDD", true));
+            this.txtDDD.Location = new System.Drawing.Point(62, 120);
+            this.txtDDD.Name = "txtDDD";
+            this.txtDDD.Size = new System.Drawing.Size(122, 20);
+            this.txtDDD.TabIndex = 4;
+            this.txtDDD.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // dataGridViewTelefone
+            // 
+            this.dataGridViewTelefone.AllowUserToAddRows = false;
+            this.dataGridViewTelefone.AllowUserToDeleteRows = false;
+            this.dataGridViewTelefone.AutoGenerateColumns = false;
+            this.dataGridViewTelefone.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewTelefone.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dDDDataGridViewTextBoxColumn,
+            this.tELEFONE1DataGridViewTextBoxColumn});
+            this.dataGridViewTelefone.DataSource = this.tELEFONEBindingSource;
+            this.dataGridViewTelefone.Location = new System.Drawing.Point(190, 91);
+            this.dataGridViewTelefone.Name = "dataGridViewTelefone";
+            this.dataGridViewTelefone.Size = new System.Drawing.Size(195, 89);
+            this.dataGridViewTelefone.TabIndex = 2;
+            this.dataGridViewTelefone.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataGridViewTelefone_KeyDown);
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.tELEFONEBindingSource, "TELEFONE1", true));
+            this.txtTelefone.Location = new System.Drawing.Point(62, 147);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(122, 20);
+            this.txtTelefone.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "(DDD):";
+            this.label1.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Telefone:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtNome
+            // 
+            this.txtNome.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.cLIENTEBindingSource, "NOME", true));
+            this.txtNome.Location = new System.Drawing.Point(63, 45);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(382, 20);
+            this.txtNome.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nome:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtID
+            // 
+            this.txtID.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.cLIENTEBindingSource, "ID", true));
+            this.txtID.Location = new System.Drawing.Point(63, 19);
+            this.txtID.Name = "txtID";
+            this.txtID.ReadOnly = true;
+            this.txtID.Size = new System.Drawing.Size(47, 20);
+            this.txtID.TabIndex = 4;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // notifyIcon2
+            // 
+            this.notifyIcon2.Text = "notifyIcon2";
+            this.notifyIcon2.Visible = true;
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(111, 249);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(73, 23);
+            this.btnAdicionar.TabIndex = 1;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(229, 249);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(73, 23);
+            this.btnEditar.TabIndex = 2;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(341, 249);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(73, 23);
+            this.btnSalvar.TabIndex = 4;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // tELEFONEBindingSource
+            // 
+            this.tELEFONEBindingSource.DataSource = typeof(AppCadastroEstagio.TELEFONE);
+            // 
+            // dDDDataGridViewTextBoxColumn
+            // 
+            this.dDDDataGridViewTextBoxColumn.DataPropertyName = "DDD";
+            this.dDDDataGridViewTextBoxColumn.HeaderText = "DDD";
+            this.dDDDataGridViewTextBoxColumn.Name = "dDDDataGridViewTextBoxColumn";
+            this.dDDDataGridViewTextBoxColumn.Width = 50;
+            // 
+            // tELEFONE1DataGridViewTextBoxColumn
+            // 
+            this.tELEFONE1DataGridViewTextBoxColumn.DataPropertyName = "TELEFONE1";
+            this.tELEFONE1DataGridViewTextBoxColumn.HeaderText = "TELEFONE";
+            this.tELEFONE1DataGridViewTextBoxColumn.Name = "tELEFONE1DataGridViewTextBoxColumn";
+            // 
+            // cLIENTEBindingSource
+            // 
+            this.cLIENTEBindingSource.DataSource = typeof(AppCadastroEstagio.CLIENTE);
+            // 
+            // iDDataGridViewTextBoxColumn
+            // 
+            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
+            this.iDDataGridViewTextBoxColumn.Width = 40;
+            // 
+            // nOMEDataGridViewTextBoxColumn
+            // 
+            this.nOMEDataGridViewTextBoxColumn.DataPropertyName = "NOME";
+            this.nOMEDataGridViewTextBoxColumn.HeaderText = "NOME";
+            this.nOMEDataGridViewTextBoxColumn.Name = "nOMEDataGridViewTextBoxColumn";
+            this.nOMEDataGridViewTextBoxColumn.Width = 180;
+            // 
+            // Deletar
+            // 
+            this.Deletar.HeaderText = "Excluir";
+            this.Deletar.Name = "Deletar";
+            this.Deletar.Text = "X";
+            this.Deletar.UseColumnTextForButtonValue = true;
+            this.Deletar.Width = 40;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 423);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridViewCliente);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnAdicionar);
+            this.Name = "Form1";
+            this.Text = "App Cadastro";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCliente)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTelefone)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tELEFONEBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cLIENTEBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labID;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridViewCliente;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.DataGridView dataGridViewTelefone;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.NotifyIcon notifyIcon2;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.BindingSource cLIENTEBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dDDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tELEFONE1DataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource tELEFONEBindingSource;
+        private System.Windows.Forms.TextBox txtDDD;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nOMEDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewButtonColumn Deletar;
+    }
+}
+
---
+++ AppCadastroEstagio/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.Infrastructure;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppCadastroEstagio
+{
+    public partial class Form1 : Form
+    {
+        CadastroEntities db;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            panel1.Enabled = false;
+            db = new CadastroEntities();
+            db.Configuration.ProxyCreationEnabled = false;
+            db.Configuration.LazyLoadingEnabled = false;
+           
+            
+            ShowTelefone();
+        }
+        private void ShowTelefone() {
+            CLIENTE obj = cLIENTEBindingSource.Current as CLIENTE;
+            if (obj != null) {
+                if (obj.TELEFONEs != null) {
+                    tELEFONEBindingSource.DataSource = obj.TELEFONEs.ToList();
+                }
+            }
+        }
+
+        private void dataGridViewCliente_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridViewCliente.Columns[e.ColumnIndex].Name == "Deletar")
+            {
+                if (MessageBox.Show("você tem certeza que deseja excluir-lo? ", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    db.CLIENTEs.Remove(cLIENTEBindingSource.Current as CLIENTE);
+                cLIENTEBindingSource.RemoveCurrent();
+            }
+        }
+
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            btnAdicionar.Enabled = false;
+            panel1.Enabled = true;
+            txtNome.Focus();
+            CLIENTE c = new CLIENTE();
+            db.CLIENTEs.Add(c);
+            cLIENTEBindingSource.Add(c);
+            cLIENTEBindingSource.MoveLast();
+           
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            btnEditar.Enabled = false;
+            panel1.Enabled = true;
+            txtNome.Focus();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            panel1.Enabled = false;
+            btnAdicionar.Enabled = true;
+            btnEditar.Enabled = true;
+            cLIENTEBindingSource.ResetBindings(false);
+
+        }
+
+        private async void btnSalvar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                await db.SaveChangesAsync();
+                panel1.Enabled = false;
+                btnAdicionar.Enabled = true;
+                btnEditar.Enabled = true;
+                MessageBox.Show("Seu cadastro foi salvo com sucesso!", "massage", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CLIENTE c = cLIENTEBindingSource.Current as CLIENTE;
+            if (c != null)
+            {
+                if (tELEFONEBindingSource.DataSource == null)
+                    tELEFONEBindingSource.DataSource = c.TELEFONEs.ToList();
+                TELEFONE t = new TELEFONE() { CLIENTE = c };
+                tELEFONEBindingSource.Add(t);
+                db.TELEFONEs.Add(t);
+            }
+        }
+
+        private void dataGridViewCliente_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                ShowTelefone();
+            }
+        }
+
+        private void dataGridViewTelefone_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                if (MessageBox.Show("você tem certeza que deseja excluir-lo? ", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    db.TELEFONEs.Remove(tELEFONEBindingSource.Current as TELEFONE);
+                tELEFONEBindingSource.RemoveCurrent();
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+    }
+
